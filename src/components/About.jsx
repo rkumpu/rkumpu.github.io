60831098fe3bf61da10e7f960e1300fdc04a0788
Roni Kumpulainen <140971053+rkumpu@users.noreply.github.com>
2023-08-22 13:52:26 +0300

    Temporarily remove 'Past work' content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import svgCoding from "../assets/coding.svg";
+
 export default function About() {
     return (
         <section id="about">
@@ -15,19 +17,21 @@ export default function About() {
                         I learned web development at age 12 along with various other programming languages. Programming is what I&apos;m most interested in, and I love creating web apps that stand out.
                     </p>
                     <div className="flex justify-center">
-                        <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                        <a href="https://twitter.com/rkumpu_" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                             Work With Me
                         </a>
+                        {/*
                         <a href="https://twitter.com/rkumpu_" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                             See My Past Work
                         </a>
+                        */}
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
                         className="object-cover object-center rounded"
                         alt=".svg file"
-                        src="./src/assets/coding.svg"
+                        src={svgCoding}
                     />
                 </div>
             </div>
